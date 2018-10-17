2499031a3497721e6b75dbe95b8baa13c816766b
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-10-17 20:02:42 +0200

    going around typescript warnings

---
+++ src/p5.d.ts
@@ -0,0 +1,8 @@
+interface p5 {}
+
+declare function text(text: string, x: number, y: number): p5
+declare function textAlign(x: number, y?: number): p5
+declare function textSize(size: number): p5
+declare function background(greyscale: number): p5
+declare function createCanvas(width: number, height: number, renderer?: object): p5
+declare var CENTER: number
