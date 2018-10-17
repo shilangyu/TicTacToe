9e0fc9f725e505cad85121c31b880a4811cdd471
shilangyu <xmarcinmarcin@gmail.com>
2018-10-17 17:09:48 +0200

    getting shit ready

--- src/brain.ts
+++ src/brain.ts
@@ -1,15 +1,39 @@
-class Brain {
-	brain: 
-
-	constructor(public source: string) {
-		fetch(source)
-			.then(res => res.json())
-			.then(json => this.)
-	}
-}
-
 interface Choice {
 	x: number,
 	y: number,
 	fitness: number
+}
+
+interface Decisions {
+	[key: string]: Choice[]
+}
+
+
+class Brain {
+	brain: Decisions;
+
+	constructor(public source: string) {
+		this.brain = {}
+
+		fetch(source)
+			.then(res => res.json())
+			.then(json => this.brain = json)
+	}
+
+	static parseBoard(board: Tile[][], player: string): string[] {
+		const stringify = (signs: string[][]) => 
+			signs.flat().map(e => {
+				if(e === player)
+					return '0'
+				else if(e !== '')
+					return '1'
+				else 
+					return 'null'
+			}).join('')
+			
+		const signs = board.map(row => row.map(({ sign }) => sign))
+		stringify(signs)
+
+		return [stringify(signs)]
+	}
 }
--- src/sketch.ts
+++ src/sketch.ts
@@ -1,3 +1,37 @@
+interface Tile {
+	x: number,
+	y: number,
+	sign: string
+}
+
+const board: Tile[][] = [
+	[{ x: 0, y: 0, sign: '' }, { x: 1, y: 0, sign: '' }, { x: 2, y: 0, sign: '' }],
+	[{ x: 0, y: 1, sign: '' }, { x: 1, y: 1, sign: '' }, { x: 2, y: 1, sign: '' }],
+	[{ x: 0, y: 2, sign: '' }, { x: 1, y: 2, sign: 'X' }, { x: 2, y: 2, sign: '' }]
+]
+
+const env = {
+	scale: 150,
+	background: 151,
+	canvasSize: {
+		x: 450,
+		y: 450
+	}
+}
+
 function setup() {
-	console.log('cunt :)')
+	createCanvas(env.canvasSize.x, env.canvasSize.y)
+	background(env.background)
+
+	textAlign(CENTER, CENTER)
+	textSize(env.scale)
+	console.log(Brain.parseBoard(board, 'X'))
+}
+
+function draw() {
+	board.forEach(row =>
+		row.forEach(({ x, y, sign }) =>
+			text(sign, x * env.scale + env.scale / 2, y * env.scale + env.scale / 2)
+		)
+	)
 }
