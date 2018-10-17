287008469b503ed601a725641d04fa90456de871
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-10-17 22:44:27 +0200

    drawing the full board

--- src/p5.d.ts
+++ src/p5.d.ts
@@ -3,6 +3,11 @@ interface p5 {}
 declare function text(text: string, x: number, y: number): p5
 declare function textAlign(x: number, y?: number): p5
 declare function textSize(size: number): p5
+declare function stroke(greyscale: number): p5
+declare function strokeWeight(size: number): p5
 declare function background(greyscale: number): p5
 declare function createCanvas(width: number, height: number, renderer?: object): p5
+declare function line(x1: number, y1: number, x2: number, y2: number): p5
+declare function noLoop(): void
+declare function redraw(amount: number): void
 declare var CENTER: number
