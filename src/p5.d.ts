dbc69adccfe3d9ffea2127c9e6bad867860427ab
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-10-18 22:33:19 +0200

    added clear canvas function

--- src/p5.d.ts
+++ src/p5.d.ts
@@ -11,4 +11,5 @@ declare function createCanvas(width: number, height: number, renderer?: object):
 declare function line(x1: number, y1: number, x2: number, y2: number): p5
 declare function noLoop(): void
 declare function redraw(amount?: number): void
+declare function clear(): void
 declare var CENTER: number
