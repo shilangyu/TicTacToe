789953a5504b20d0d824fe83e0a17458473feb9c
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-10-18 19:08:59 +0200

    drawing and click event moved to Board class

--- src/Board.ts
+++ src/Board.ts
@@ -1,10 +1,40 @@
 class Board {
 	tiles: Tile[][]
-	constructor(public width: number, public height: number) {
-		this.tiles = new Array(height).fill(null).map((e, y) =>
-			new Array(width).fill(null).map((e, x) =>
+
+	constructor(public width: number, public height: number, public target?: HTMLCanvasElement) {
+		this.tiles = new Array(height).fill(null).map((_, y) =>
+			new Array(width).fill(null).map((_, x) =>
 				({ x, y, sign: null })
 			)
 		)
+
+		if (target)
+			this.addEventListener()
+	}
+
+	draw(signs: { AI: string, player: string }) {
+		this.tiles.forEach(row =>
+			row.forEach(({ x, y, sign }) => {
+				const txt = sign === null ? '' : sign === 0 ? signs.player : signs.AI
+				text(txt, env.scale * (x + 1 / 2), env.scale * (y + 1 / 2))
+				line(env.scale * x, 0, env.scale * x, env.canvasSize.y)
+				line(0, env.scale * y, env.canvasSize.x, env.scale * y)
+			})
+		)
+	}
+
+	private addEventListener() {
+		(this.target as HTMLCanvasElement).addEventListener('click', ({ clientX: x, clientY: y, target }) => {
+			const { clientHeight: canvasHeight, clientWidth: canvasWidth } = (target as HTMLElement)
+
+			const grid = [x / (canvasWidth / this.width), y / (canvasHeight / this.height)].map(Math.floor)
+
+			this.tiles[grid[1]][grid[0]].sign = 0
+		})
+	}
+
+	set canvas(ele: HTMLCanvasElement) {
+		this.target = ele
+		this.addEventListener()
 	}
 }
--- src/sketch.ts
+++ src/sketch.ts
@@ -7,7 +7,7 @@ const env = {
 	},
 	signs: {
 		AI: 'o',
-		player: 'x'
+		player: '×'
 	}
 }
 
@@ -17,25 +17,18 @@ const brain = new Brain('/decision.json')
 function setup() {
 	createCanvas(env.canvasSize.x, env.canvasSize.y)
 	background(env.background)
-
+	
 	textAlign(CENTER, CENTER)
 	textSize(env.scale)
 	strokeWeight(15)
 	stroke(255)
 	fill(255)
-
-	noLoop()
+	
+	// noLoop()
+	board.canvas = document.querySelector('canvas') || document.createElement('canvas')
 	console.log(Brain.parseBoard(board.tiles))
 }
 
 function draw() {
-	board.tiles.forEach(row =>
-		row.forEach(({ x, y, sign }) => {
-			const txt = sign === null ? '' : sign === 0 ? env.signs.player : env.signs.AI
-			text(txt, env.scale * (x + 1 / 2), env.scale * (y + 1 / 2))
-			line(env.scale * x, 0, env.scale * x, env.canvasSize.y)
-			line(0, env.scale * y, env.canvasSize.x, env.scale * y)
-		})
-	)
-
+	board.draw(env.signs)
 }
