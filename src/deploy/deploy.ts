e616fa8b87ef93874424d029c6c2257cbc6caaf7
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-10-21 13:37:04 +0200

    deploy moved to seperate folder

--- src/deploy/deploy.ts
+++ src/deploy/deploy.ts
@@ -3,14 +3,31 @@ import * as fs from 'fs'
 import * as os from 'os'
 import * as path from 'path'
 
-import { productionDecision } from './helper'
+const productionDecision = (): string => {
+	const decs = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'trainer', 'decision.json')).toString()) as Decisions
 
-const pjoin = (...strings: string[]) => path.join(__dirname, ...strings)
+	const parsedDecisions: Decisions = {}
+
+	for (const key of Object.keys(decs)) {
+		const best = (decs[key] as FitGuess[]).sort((a, b) => b.fitness - a.fitness)[0]
+
+		parsedDecisions[key] = {
+			x: best.x,
+			y: best.y
+		}
+	}
+
+	return JSON.stringify(parsedDecisions)
+}
+
+const pjoin = (...strings: string[]) => path.join(__dirname, '..', ...strings)
 
 fs.mkdtemp(os.tmpdir(), (err, tempPath) => {
 	if (err) throw err
 
+	cp.execSync(`git fetch`)
 	cp.execSync(`git worktree add "${tempPath}" gh-pages`)
+	cp.execSync(`git pull`)
 
 	fs.readdirSync(pjoin('..', 'build', 'preview'))
 		.forEach(file => fs.copyFileSync(pjoin('..', 'build', 'preview', file), path.join(tempPath, file)))
@@ -20,6 +37,6 @@ fs.mkdtemp(os.tmpdir(), (err, tempPath) => {
 
 	cp.execSync(`cd "${tempPath}" && git add --all && git commit -m "${new Date().toLocaleString()}" && git push origin gh-pages && cd "${__dirname}"`)
 
-	cp.execSync(`git worktree prune`)
 	fs.unlinkSync(tempPath)
+	cp.execSync(`git worktree prune`)
 })
