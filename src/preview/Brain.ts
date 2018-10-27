f3d4946757be87da565fd5a5fa002a36c9ee983f
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-10-27 17:09:59 +0200

    devmode creates decisions when unavailable

--- src/preview/Brain.ts
+++ src/preview/Brain.ts
@@ -48,9 +48,33 @@ export default class Brain {
 			}
 		}
 
+		if(this.mode === 'dev') {
+			this.createMoves(rotations[0])
+			return this.decide(rotations, mirrors)
+		}
+
 		return {
 			x: -1 as Coord,
 			y: -1 as Coord
 		}
+	}
+
+	private createMoves(board: string) {
+		this.brain[board] = []
+		const dec = this.brain[board] as FitGuess[]
+
+		let specimen = board.replace(/[10]/g, '    ')
+		if(specimen.indexOf('null') === -1) return
+
+		do {
+			let currField = specimen.lastIndexOf('null') / 4
+			dec.push({
+				x: Math.floor(currField / 3) as Coord,
+				y: currField % 3 as Coord,
+				fitness: 100
+			})
+
+			specimen = specimen.replace(/null(?! +)$/, '    ')
+		} while(specimen.indexOf('null') !== -1)
 	}
 }
