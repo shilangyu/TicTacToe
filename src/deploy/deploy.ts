e44ee2054375f8cd017d02dde68637e59c5f2e52
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-10-29 00:05:55 +0100

    temp folder is created in the project

--- src/deploy/deploy.ts
+++ src/deploy/deploy.ts
@@ -1,15 +1,12 @@
 import * as cp from 'child_process'
 import * as fs from 'fs'
-import * as os from 'os'
 import * as path from 'path'
 
 const pjoin = (...strings: string[]) => path.join(__dirname, '..', ...strings)
 
-fs.mkdtemp(os.tmpdir(), (err, tempPath) => {
+fs.mkdtemp('temp', (err, tempPath) => {
 	if (err) throw err
 
-	cp.execSync(`git fetch`)
-	cp.execSync(`git pull`)
 	cp.execSync(`git worktree add "${tempPath}" gh-pages`)
 
 	fs.readdirSync(pjoin('..', 'public'))
