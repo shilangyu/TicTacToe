f1781750209d4990d5aa9e9778dee529792347e4
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-10-28 23:30:07 +0100

    [refactor] cleaning up the helper from useless loops

--- src/helper.ts
+++ src/helper.ts
@@ -1,67 +1,39 @@
-export const rotate = (matrix: any[][], how: number): any[][] => {
-	const N = matrix.length - 1
-	if(how === 0) {
-		return [...matrix.map(e => [...e])]
-	}
-	let result = matrix.map((row, i) =>
-		row.map((_, j) => matrix[N - j][i])
-	)
+export const rotate = (matrix: any[][], amount: number): any[][] => {
+	let copy = matrix.map(row => row.map(e => e))
 
-	for (let i = 1; i < how; i++) {
-		result = result.map((row, i) =>
-			row.map((_, j) => matrix[N - j][i])
-		)
-	}
-	matrix.length = 0
-	matrix.push(...result)
-	return matrix
+	return copy.map((row, i) => row.map((_, j) => {
+		let [x, y] = mapxy(i as Coord, j as Coord, amount, 'rotation')
+		return matrix[x][y]
+	}))
 }
 
-export const mirror = (matrix: any[][], how: number): any[][] => {
-	const copy = matrix.map(row => row.map(e => Array.isArray(e) ? [...e] : e instanceof Object ? { ...e } : e))
+export const mirror = (matrix: any[][], amount: number): any[][] => {
+	const copy = matrix.map(row => row.map(e => e))
+	let mapper: (x: Coord, y: Coord) => any
 
-	switch (how) {
+	switch (amount) {
 		case 0:
-			return matrix.map((row, i) =>
-				row.map((_, j) => {
-					const x = matrix.length - j - 1
-					const y = matrix[i].length - i - 1
-					return copy[x][y]
-				})
-			)
+			mapper = (x, y) => copy[matrix.length - y - 1][matrix[x].length - x - 1]
+			break
 
 		case 1:
-			return copy.map((row, i) =>
-				row.map((_, j) => {
-					const x = j
-					const y = i
-					return copy[x][y]
-				})
-			)
+			mapper = (x, y) => copy[y][x]
+			break
 
 		case 2:
-			return copy.map((row, i) =>
-				row.map((_, j) => {
-					const x = i
-					const y = matrix[i].length - j - 1
-					return copy[x][y]
-				})
-			)
+			mapper = (x, y) => copy[x][matrix[x].length - y - 1]
+			break
 
 		case 3:
-			return copy.map((row, i) =>
-				row.map((_, j) => {
-					const x = matrix.length - i - 1
-					const y = j
-					return copy[x][y]
-				})
-			)
+			mapper = (x, y) => copy[matrix.length - x - 1][y]
+			break
 	}
-	return Array(Array())
+
+	return matrix.map((row, i) => row.map((_, j) => mapper(i as Coord, j as Coord)))
 }
 
 export const mapxy = (x: Coord, y: Coord, amount: number, what: 'rotation' | 'mirror'): Coord[] => {
-	if(what === 'rotation') {
+	if (what === 'rotation') {
 		x -= 1
 		y -= 1
 		const ang = (Math.PI / 2) * amount
@@ -74,13 +46,13 @@ export const mapxy = (x: Coord, y: Coord, amount: number, what: 'rotation' | 'mi
 		switch (amount) {
 			case 0:
 				return [2 - y as Coord, 2 - x as Coord]
-	
+
 			case 1:
 				return [y, x]
 
 			case 2:
 				return [x, 2 - y as Coord]
-	
+
 			case 3:
 				return [2 - x as Coord, y]
 		}
@@ -89,29 +61,7 @@ export const mapxy = (x: Coord, y: Coord, amount: number, what: 'rotation' | 'mi
 }
 
 export const unmapxy = (x: Coord, y: Coord, amount: number, what: 'rotation' | 'mirror'): Coord[] => {
-	if(what === 'rotation') {
-		x -= 1
-		y -= 1
-		const ang = (Math.PI / 2) * (4 - amount)
-		const cos = Math.cos(ang)
-		const sin = Math.sin(ang)
-		const rotatedX = Math.round(x * cos - y * sin) + 1 as Coord
-		const rotatedY = Math.round(x * sin + y * cos) + 1 as Coord
-		return [rotatedX, rotatedY]
-	} else {
-		switch (amount) {
-			case 0:
-				return [2 - y as Coord, 2 - x as Coord]
-	
-			case 1:
-				return [y, x]
-
-			case 2:
-				return [x, 2 - y as Coord]
-	
-			case 3:
-				return [2 - x as Coord, y]
-		}
-	}
-	return [1]
+	if (what === 'rotation')
+		amount = 4 - amount
+	return mapxy(x, y, amount, what)
 }
