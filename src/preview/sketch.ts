ca1143b6068d214dcb4a7cc0f4819800ee7e85aa
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-11-02 14:57:31 +0100

    removed p5, using vanilla js

--- src/preview/sketch.ts
+++ src/preview/sketch.ts
@@ -3,15 +3,17 @@ import Board from '../Board'
 
 const env = {
 	scale: 150,
-	background: 151,
+	background: '#9198a3',
+	strokes: '#ffffff',
 	msg: document.querySelector('#msg') as HTMLSpanElement,
 	restart: document.querySelector('#restart') as HTMLButtonElement,
 	canvas: {
-		ref: document.createElement('canvas'),
+		ref: document.querySelector('canvas') as HTMLCanvasElement,
 		size: {
 			x: 450,
 			y: 450
-		}
+		},
+		ctx:(document.querySelector('canvas') as HTMLCanvasElement).getContext('2d') as CanvasRenderingContext2D
 	},
 	starting: function (): () => string {
 		let nodes = Array.from(document.querySelectorAll('input[name=starting]')) as HTMLInputElement[]
@@ -22,26 +24,24 @@ const env = {
 					return radio.value
 			return ''
 		}
-	}()
+	}(),
+	fps: 60
 }
 
 let board: Board
 const brain = new Brain()
 
-;(window as any).setup = async function () {
+async function setup () {
 	restartGame()
+
 	brain.brain = await (await fetch('./decisions.json')).json()
 
-	createCanvas(env.canvas.size.x, env.canvas.size.y)
-	background(env.background)
+	env.canvas.ref.width = env.canvas.size.x
+	env.canvas.ref.height = env.canvas.size.y
 
-	textAlign(CENTER, CENTER)
-	textSize(env.scale)
-	strokeWeight(15)
-	stroke(255)
-	fill(255)
+	env.canvas.ctx.textAlign = 'center'
+	env.canvas.ctx.textBaseline = 'middle'
 
-	env.canvas.ref = document.querySelector('canvas') as HTMLCanvasElement
 
 	env.canvas.ref.addEventListener('click', ({ layerX: x, layerY: y, target }) => {
 		const { clientHeight: canvasHeight, clientWidth: canvasWidth } = (target as HTMLElement)
@@ -64,20 +64,36 @@ const brain = new Brain()
 
 	env.restart.addEventListener('click', restartGame)
 }
+setup()
 
-;(window as any).draw = function () {
-	background(env.background)
-	const { scale, canvas: { size } } = env
+
+setInterval(() => {
+	const { scale, canvas: { size, ref, ctx } } = env
+	ctx.fillStyle = env.background
+	ctx.fillRect(0, 0, ref.width, ref.height)
 
 	board.tiles.forEach((row, y) =>
 		row.forEach((sign , x) => {
 			const txt = sign === null ? '' : sign === 0 ? board.signs.player : board.signs.AI
-			text(txt, scale * (x + 1 / 2), scale * (y + 1 / 2))
-			line(scale * x, 0, scale * x, size.y)
-			line(0, scale * y, size.x, scale * y)
+			ctx.fillStyle = env.strokes
+			ctx.font = `${scale}px sans-serif`
+			ctx.fillText(txt, scale * (x + 1 / 2), scale * (y + 1 / 2))
+
+			ctx.lineWidth = env.scale / 10
+			ctx.strokeStyle = env.strokes
+
+			ctx.beginPath()
+
+			ctx.moveTo(scale * x, 0)
+			ctx.lineTo(scale * x, size.y)
+			ctx.stroke()
+			
+			ctx.moveTo(0, scale * y)
+			ctx.lineTo(size.x, scale * y)
+			ctx.stroke()
 		})
 	)
-}
+}, 1000 / env.fps)
 
 function endGame(winner: Sign) {
 	env.msg.innerHTML = winner === 0 ? 'You won' : winner === 1 ? 'AI won' : 'Draw'
