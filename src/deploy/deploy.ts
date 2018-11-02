45ff7f04f211f95ed25c679e9f867a43534d222b
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-11-02 15:36:46 +0100

    fixed folder removal

--- src/deploy/deploy.ts
+++ src/deploy/deploy.ts
@@ -2,18 +2,27 @@ import * as cp from 'child_process'
 import * as fs from 'fs'
 import * as path from 'path'
 
-const pjoin = (...strings: string[]) => path.join(__dirname, '..', ...strings)
+const rootDir = (...strings: string[]) => path.join(__dirname, '..', '..', ...strings)
 
 fs.mkdtemp('temp', (err, tempPath) => {
 	if (err) throw err
 
 	cp.execSync(`git worktree add "${tempPath}" gh-pages`)
 
-	fs.readdirSync(pjoin('..', 'public'))
-		.forEach(file => fs.copyFileSync(pjoin('..', 'public', file), path.join(tempPath, file)))
+	fs.readdirSync(rootDir('public')).forEach(file =>
+		fs.copyFileSync(rootDir('public', file), path.join(tempPath, file))
+	)
 
-	cp.execSync(`cd "${tempPath}" && git add --all && git commit -m "${new Date().toLocaleString()}" && git push origin gh-pages && cd "${__dirname}"`)
+	cp.execSync(
+		`cd "${tempPath}" && git add --all && git commit -m "${new Date().toLocaleString()}" && git push origin gh-pages && cd "${__dirname}"`
+	)
 
-	fs.unlinkSync(tempPath)
+	fs.readdirSync(tempPath)
+		.map(file => path.join(tempPath, file))
+		.forEach(path => {
+			fs.unlinkSync(path)
+		})
+	fs.rmdirSync(tempPath)
+
 	cp.execSync(`git worktree prune`)
 })
